a9b52d61708be547bcc68effd76c6a283b5fded8
unknown <yunikashakya082@gmail.com>
2023-07-20 19:12:54 +0545

    fetched and displayed the api product data into ui

--- online-store/src/components/products/Products.jsx
+++ online-store/src/components/products/Products.jsx
@@ -16,6 +16,7 @@ const Products = () => {
 
             });
     }, []);
+
     return (
         <div className='container my-5 py-5'>
             <div className='row'>
@@ -25,21 +26,40 @@ const Products = () => {
                 </div>
             </div>
 
-            <div className='row justify-content-center'>
-            <div className='image-content'>
-        {productList.length > 0 ? productList.map((product) => {
-          return (
-            <div key={product.id}>
-              <img src={product.image}
-                style={{ height: "250px", width: "250px", objectFit: "cover" }}
-                alt={product.description}
-              />
-              <br />
-              {product.description ? product.description.substring(0, 20) : 'images'}
+            <div className="buttons d-flex justify-content-center mb-5 pb-5">
+                <button className='btn btn-outline-dark me-2'>All</button>
+                <button className='btn btn-outline-dark me-2'>Men's Clothing</button>
+                <button className='btn btn-outline-dark me-2'>Women's Clothing</button>
+                <button className='btn btn-outline-dark me-2'>Electronics</button>
             </div>
-          )
-        }): isLoading ? "Loading..." : 'No Products Found!!!'}
-      </div>
+
+
+            <div className='row'>
+                {productList.length > 0 ? productList.map((product) => {
+                    return (
+                        <div className="col-md-3 mb-4">
+                            <div className="card" key={product.id}>
+                                <img src={product.image}
+                                    style={{ height: "250px", width: "250px", objectFit: "cover" }}
+                                    alt={product.description}
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title">
+                                        {product.title}
+                                    </h5>
+                                    <p className="card-text">
+                                        ${product.price}
+                                    </p>
+                                    {product.description ? product.description.substring(0, 100) : 'products'}
+                                    <br />
+                                    <br />
+                                    <a href="" className='btn btn-outline-dark'>Buy Now</a>
+                                </div>
+
+                            </div>
+                        </div>
+                    )
+                }) : isLoading ? "Loading..." : 'No Products Found!!!'}
             </div>
         </div>
     )
